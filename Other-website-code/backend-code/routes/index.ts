8225e24e4ad0f2f5cb95cdce8bf9b79955e3e3fe
eyus00 <155847959+eyus00@users.noreply.github.com>
2025-09-05 19:46:59 +0300

    cleaned up some stuff

--- Other-website-code/backend-code/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { version } from '~/utils/config';
-export default defineEventHandler(event => {
-  return {
-    message: `Backend is working as expected (v${version})`,
-  };
-});
--- Other-website-code/backend-code/routes/metrics/providers.put.ts
+++
@@ -1,2 +0,0 @@
-// Redirect to the POST handler which now supports both methods
-export { default } from './providers.post';
